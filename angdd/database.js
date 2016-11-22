b2136d6bc3e44ad37338a7565a615ac0a21060e3
rgyann <robertgyann@gmail.com>
2016-11-22 07:11:34 -0500

    Update database.js

--- angdd/database.js
+++ angdd/database.js
@@ -2,15 +2,12 @@
 
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/angdd', function(err) {
+mongoose.connect('mongodb://localhost/posts', function(err) {
   if(err) {
-    console.log('Failed connecting to Mongodb!');
+    console.log('Failed connecting to MongoDB!');
   } else {
-    console.log('Successfully connected to Mongo!');
+    console.log('Successfully connected to MongoDB!');
   }
 });
 
-module.exports = {
-    url : 'mongodb://localhost/'
-};
 
